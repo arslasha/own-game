782aa89ae54834130fceca5dad252666d3da00cd
arslasha <aliernhis@gmail.com>
2026-09-17 07:43:17 +0300

    feat: add static assets and questions data

---
+++ src/data/questions-aisha.js
@@ -0,0 +1,224 @@
+export const categories = [
+  {
+    title: "👀 Кто они",
+    questions: [
+      { 
+        points: 100, 
+        q: "Кто на этом фото?", 
+        image: "public/img/kto-oni-100.png", 
+        a: "Аиша + Арслан" 
+      },
+      { 
+        points: 200, 
+        q: "Кто на этом фото?",
+        image: "public/img/kto-oni-200.png", 
+        a: "Никита + Полина" 
+      },
+      { 
+        points: 300, 
+        q: "Кто на этом фото?",
+        image: "public/img/kto-oni-300.png", 
+        a: "Амина + Малика" 
+      },
+      { 
+        points: 400, 
+        q: "Кто на этом фото?",
+        image: "public/img/kto-oni-400.png", 
+        a: "Ксюша + Назар" 
+      },
+      { 
+        points: 500, 
+        q: "Кто на этом фото?",
+        image: "/public/img/kto-oni-500.png",
+        a: "Марина + Артем" 
+      }
+    ]
+  },
+  {
+    title: "🤺 Колит в самое сердце",
+    questions: [
+      { 
+        points: 100, 
+        q: "Шпага, рапира или сабля?", 
+        image: "public/img/kolit-100.png", 
+        a: "Шпага" 
+      },
+      { 
+        points: 200, 
+        q: "Шпага, рапира или сабля?", 
+        image: "public/img/kolit-200.png", 
+        a: "Сабля" 
+      },
+      { 
+        points: 300, 
+        q: "Шпага, рапира или сабля?", 
+        image: "public/img/kolit-300.png", 
+        a: "Шпага" 
+      },
+      { 
+        points: 400, 
+        q: "Шпага, рапира или сабля?", 
+        image: "public/img/kolit-400.jpg", 
+        a: "Рапира" 
+      },
+      { 
+        points: 500, 
+        q: "Шпага, рапира или сабля?", 
+        image: "public/img/kolit-500.png", 
+        a: "Рапира" 
+      }
+    ]
+  },
+  {
+    title: "🤡 Зумерские мемы",
+    questions: [
+      { 
+        points: 100, q: "Что написано на картинке?",
+        a: "Очень необычно зайка",
+        image: "public/img/mem-100.jpg",
+        aimage: "public/img/mem-100-a.jpg"
+      },
+      { 
+        points: 200, q: "Что написано на картинке?",
+        a: "Напакостничал)))", 
+        image: "public/img/mem-200.jpg",
+        aimage: "public/img/mem-200-a.jpg"
+      },
+      { 
+        points: 300, q: "Что написано на картинке?",
+        a: "Веселый", 
+        image: "public/img/mem-300.jpg",
+        aimage: "public/img/mem-300-a.jpg"
+      },
+      { 
+        points: 400, q: "Что написано на картинке?",
+        a: "Я смотрю ты ни бе ни ме", 
+        image: "public/img/mem-400.jpg",
+        aimage: "public/img/mem-400-a.jpg"
+      },
+      { 
+        points: 500, q: "Что написано на картинке?",
+        a: "Все хорошо казахи ушли", 
+        image: "public/img/mem-500.jpg",
+        aimage: "public/img/mem-500-a.jpg"
+      },
+        
+      
+    ]
+  },
+  {
+    title: "💅 Кто не ответит в чс",
+    questions: [
+      { points: 100, q: "Аиша начинала вести блог чему он был посвящен?", 
+        options: ["Фитнес", "Стрип", "Реабилитация", "Бьюти"],
+        a: "A) Фитнес" },
+      { points: 200, q: "В скольки школах училась?",
+        options: ["В двух", "В трех", "В четырех", "В пяти"],
+        a: "B) В трех" },
+      { points: 300, q: "Во сколько Аиша впервые поехала в лагерь?",
+        options: ["5 лет", "6 лет", "7 лет", "8 лет"],
+        a: "C) 7 лет" },
+      { points: 400, q: "Какими танцами Аиша занималась после художественной гимнастики?",
+        options: ["Татарские народные", "Классические", "Стрип", "Хип-хоп"],
+        a: "A) Татарские народные" },
+      { points: 500, q: "Как родители Аиши изначально хотели ее назвать?", 
+        options: ["Аиша", "Мария", "Женя", "Малика"],
+        a: "A) Аиша" }
+    ]
+  },
+  {
+    title: "📸 обход цензуры",
+    questions: [
+      { points: 100, q: "На чем сидит Аиша?", 
+        image: "public/img/cenz-100.jpg",
+        aimage: "public/img/cenz-100-a.jpg",
+        options: ["Конь", "Верблюд", "Осел", "LADA Granta"],  
+        a: "B)Верблюд" 
+      },
+      { points: 200, 
+        q: "Что у Аиши в руках?", 
+        image: "public/img/cenz-200.jpg",
+        aimage: "public/img/cenz-200-a.jpg",
+        options: ["Кубок и диплом", "Сертификат и медаль", "Диплом и сертификат", "Рапира и кубок"],
+        a: "A) Кубок и диплом" 
+      },
+      { points: 300, q: "Что написано у Аиши на руке?",
+        image: "public/img/cenz-300.jpg",
+        aimage: "public/img/cenz-300-a.jpg",
+        options: ["be brave", "sé valiente", "sois courageux", "sei coraggioso"],
+        a: "B) sé valiente" 
+      },
+      { points: 400, q: "Что за кроссовки?",
+        image: "public/img/cenz-400.jpg",
+        aimage: "public/img/cenz-400-a.jpg",
+        options: ["Адидас", "Нью бэленс", "Найк", "Рибок"],
+        a: "C) Найк" 
+      },
+      { points: 500, q: "Какая курилка у Аиши в руке?", 
+        image: "public/img/cenz-500.jpg",
+        aimage: "public/img/cenz-500-a.jpg",
+        options: ["Pafos", "Lost Marry", "HQD", "Waka"],
+        a: "D) Waka" }
+    ]
+  },
+  {
+    title: "🎥 я снимаю",
+    questions: [
+      { points: 100, 
+        q: "Кот в мешке", 
+        a: "Минет" 
+      },
+      { points: 200, 
+        q: "Что за песня?", 
+        a: "Наколочки",
+        video: "public/video/vid-200.mp4",
+        avideo: "public/video/vid-200-a.mp4",
+        options: ["Наколочки", "Гоп-стоп", "Алеха-гавая", "Золотые купола"],
+      },
+      { points: 300, 
+        q: "Что назар сделает дальше?", 
+        video: "public/video/vid-300.mp4",
+        avideo: "public/video/vid-300-a.mp4",
+        options: ["Толкнет Самиру", "Начнет танцевать брэйк", "Упадет", "Покажет ДЦП жест"],
+        a: "B)Начнет танцевать брэйк" 
+      },
+      
+      { points: 400, q: "Что произойдет дальше?",
+        video: "public/video/vid-400.mp4",
+        avideo: "public/video/vid-400-a.mp4",
+        options: ["Нас ловит береговая охрана", "На аишу летит огромный жук", "Арслан показывает жопу", "Аиша падает в воду"],
+        a: "C) Арслан показывает жопу" 
+      },
+      { points: 500, q: "Какая музыка играет на видео?",
+         video: "public/video/vid-500.mp4",
+         avideo: "public/video/vid-500-a.mp4",
+         options: ["Мама я в Дубае", "Гимн Круговорота", "Тиктоник", "Утэ папа утэ"],
+         a: "A) Мама я в Дубае" }
+    ]
+  },
+  {
+    title: "🥸 Элордина",
+    questions: [
+      { points: 100, q: "Из какого фильма кадр?", 
+        a: "Эйфория",
+        image: "public/img/crash-100.jpg",
+      },
+      { points: 200, q: "Из какого фильма кадр?", 
+        a: "Грозовой перевал",
+        image: "public/img/crash-200.jpg",
+      },
+      { points: 300, q: "Из какого фильма кадр?", 
+        a: "Солтберн",
+        image: "public/img/crash-300.jpg",
+      },
+      { points: 400, q: "Из какого фильма кадр?", 
+        a: "Элвис Пресли",
+        image: "public/img/crash-400.jpg",
+      },
+      { points: 500, q: "Из какого фильма кадр?", 
+        a: "О Канаде", 
+        image: "public/img/crash-500.jpg",
+      }
+    ]
+  }
+];
---
+++ src/data/questions.js
@@ -0,0 +1,75 @@
+// Template questions file for new games
+// Provides 7 categories, each with 5 placeholder questions (100‑500 points).
+
+export const categories = [
+  {
+    title: "Литература",
+    questions: [
+      { points: 100, q: "Кто написал роман 'Война и мир'?", image: "public/img/placeholder.jpg", a: "Лев Толстой", video: "" },
+      { points: 200, q: "Кто автор поэмы 'Евгений Онегин'?", image: "public/img/placeholder.jpg", a: "Александр Пушкин", video: "" },
+      { points: 300, q: "Кто написал 'Преступление и наказание'?", image: "public/img/placeholder.jpg", a: "Фёдор Достоевский", video: "" },
+      { points: 400, q: "Кто написал 'Мастер и Маргарита'?", image: "public/img/placeholder.jpg", a: "Михаил Булгаков", video: "" },
+      { points: 500, q: "Кто автор сказки 'Морозко'?", image: "public/img/placeholder.jpg", a: "Алексей Толстой", video: "" }
+    ]
+  },
+  {
+    title: "География",
+    questions: [
+      { points: 100, q: "Какая самая длинная река в мире?", image: "public/img/placeholder.jpg", a: "Нил", video: "" },
+      { points: 200, q: "Столица Японии?", image: "public/img/placeholder.jpg", a: "Токио", video: "" },
+      { points: 300, q: "Самая высокая гора Земли?", image: "public/img/placeholder.jpg", a: "Эверест", video: "" },
+      { points: 400, q: "Какая страна самая большая по площади?", image: "public/img/placeholder.jpg", a: "Россия", video: "" },
+      { points: 500, q: "Какой океан самый большой?", image: "public/img/placeholder.jpg", a: "Тихий океан", video: "" }
+    ]
+  },
+  {
+    title: "История",
+    questions: [
+      { points: 100, q: "Год начала Второй мировой войны?", image: "public/img/placeholder.jpg", a: "1939", video: "" },
+      { points: 200, q: "Кто был первым президентом США?", image: "public/img/placeholder.jpg", a: "Джордж Вашингтон", video: "" },
+      { points: 300, q: "Какой год начались реформы Петра I?", image: "public/img/placeholder.jpg", a: "1698", video: "" },
+      { points: 400, q: "Кто возглавлял СССР в 1945 году?", image: "public/img/placeholder.jpg", a: "Иосиф Сталин", video: "" },
+      { points: 500, q: "Какой древний город был разрушен извержением Везувия?", image: "public/img/placeholder.jpg", a: "Помпеи", video: "" }
+    ]
+  },
+  {
+    title: "Наука",
+    questions: [
+      { points: 100, q: "Какой элемент имеет символ O?", image: "public/img/placeholder.jpg", a: "Кислород", video: "" },
+      { points: 200, q: "Кто разработал теорию относительности?", image: "public/img/placeholder.jpg", a: "Альберт Эйнштейн", video: "" },
+      { points: 300, q: "Что измеряется в джоулях?", image: "public/img/placeholder.jpg", a: "Энергия", video: "" },
+      { points: 400, q: "Какая планета самая большая в Солнечной системе?", image: "public/img/placeholder.jpg", a: "Юпитер", video: "" },
+      { points: 500, q: "Кто открыл закон всемирного тяготения?", image: "public/img/placeholder.jpg", a: "Ньютон", video: "" }
+    ]
+  },
+  {
+    title: "Спорт",
+    questions: [
+      { points: 100, q: "Сколько игроков в футбольной команде на поле?", image: "public/img/placeholder.jpg", a: "11", video: "" },
+      { points: 200, q: "Какой спорт считается \"королем спорта\"?", image: "public/img/placeholder.jpg", a: "Футбол", video: "" },
+      { points: 300, q: "В какой стране прошли первые Олимпийские игры современного формата?", image: "public/img/placeholder.jpg", a: "Греция", video: "" },
+      { points: 400, q: "Какая команда выиграла чемпионат мира по футболу 2018?", image: "public/img/placeholder.jpg", a: "Франция", video: "" },
+      { points: 500, q: "Кто известен как \"Король баскетбола\"?", image: "public/img/placeholder.jpg", a: "Майкл Джордан", video: "" }
+    ]
+  },
+  {
+    title: "Искусство",
+    questions: [
+      { points: 100, q: "Какой художник нарисовал 'Мону Лизу'?", image: "public/img/placeholder.jpg", a: "Леонардо да Винчи", video: "" },
+      { points: 200, q: "Кто написал балет 'Лебединое озеро'?", image: "public/img/placeholder.jpg", a: "Пётр Чайковский", video: "" },
+      { points: 300, q: "Какой стиль живописи известен яркими мазками?", image: "public/img/placeholder.jpg", a: "Импрессионизм", video: "" },
+      { points: 400, q: "Кто создал скульптуру 'Давид'?", image: "public/img/placeholder.jpg", a: "Микеланджело", video: "" },
+      { points: 500, q: "Какой фильм получил Оскар за лучший фильм 2022 года?", image: "public/img/placeholder.jpg", a: "CODA", video: "" }
+    ]
+  },
+  {
+    title: "Технологии",
+    questions: [
+      { points: 100, q: "Какой язык программирования используется для веб‑разработки?", image: "public/img/placeholder.jpg", a: "JavaScript", video: "" },
+      { points: 200, q: "Что обозначает аббревиатура AI?", image: "public/img/placeholder.jpg", a: "Artificial Intelligence", video: "" },
+      { points: 300, q: "Как называется технология блокчейн?", image: "public/img/placeholder.jpg", a: "Блокчейн", video: "" },
+      { points: 400, q: "Кто основал компанию Apple?", image: "public/img/placeholder.jpg", a: "Стив Джобс", video: "" },
+      { points: 500, q: "Какая компания создала процессор Ryzen?", image: "public/img/placeholder.jpg", a: "AMD", video: "" }
+    ]
+  }
+];
